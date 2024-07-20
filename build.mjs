e61658cdfa3a1fe7a39e78f8589477a4200cd88b
timothycarambat <rambat1010@gmail.com>
2024-07-19 17:02:06 -0700

    fix build output - cjs

--- build.mjs
+++ build.mjs
@@ -4,11 +4,11 @@ import fs from 'fs';
 if (fs.existsSync('./dist')) fs.rmSync('./dist', { recursive: true });
 console.log(`esbuild: Bundling package`);
 await esbuild.build({
-  entryPoints: ['index.js'],
+  entryPoints: ['src/index.js'],
   bundle: true,
   outfile: 'dist/index.js',
   platform: 'node',
-  format: 'esm',
+  format: 'cjs',
   packages: 'external',
-  minify: true,
+  minify: false,
 })
